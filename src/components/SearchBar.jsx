5034d30efdecaacf04956ec1cdd8372dac86c939
tontonel <tionut80@gmail.com>
2024-06-13 19:20:17 +0300

    added absolute position for search options

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,31 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 
 const SearchBar = (props) => {
 
     const inputRef = useRef(null);
 
+    const optionRef = useRef(null);
+
+    const [showOptions, setShowOptions] = useState(false);
+
     const handleOptionClick = (option) => {
         inputRef.current.value = '';
         props.handleOptionClick(option);
     }
+    
+    const handleClickOutside = (event) => {
+        if (inputRef.current && !inputRef.current.contains(event.target)
+            && optionRef.current && !optionRef.current.contains(event.target)) {
+            setShowOptions(false);
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
 
     return (
     <div className='search-bar'>
@@ -16,14 +34,20 @@ const SearchBar = (props) => {
             type="text"
             placeholder="Search for a user..."
             onChange={props.debouncedSearch}
+            onFocus={() => setShowOptions(true)}
+
         />
-        <ul>
-            {props.userGists !== undefined &&
-            props.userGists.length !== 0 ?
-                props.userGists.map((option) => (
-                <li key={option.id} onClick={() => handleOptionClick(option)}>gist:{option.id}</li>))
-            : <li>No results to show</li>}
-        </ul>
+        { showOptions && 
+            <ul
+                ref={optionRef}
+                className='search-options'
+            >
+                {props.userGists !== undefined &&
+                props.userGists.length !== 0 ?
+                    props.userGists.map((option) => (
+                    <li key={option.id} onClick={() => handleOptionClick(option)}>gist:{option.id}</li>))
+                : <li>No results to show</li>}
+            </ul>}
     </div>
     );
 };
